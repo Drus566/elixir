6051576c190eb8003d38965c53e12c03ee1ec494
Drus <Drus@mail>
2026-01-27 22:53:23 +0500

    ch

--- syntax/hospital/lib/hospital.ex
+++ syntax/hospital/lib/hospital.ex
@@ -8,3 +8,32 @@ defmodule Hospital do
     end
   end
 end
+
+# defmodule Hospital.Registry do
+#   alias Hospital.Validator
+
+#   def start do
+#     # Запускаем процесс и передаем ему начальное состояние — 0
+#     spawn(fn -> loop(0) end)
+#   end
+
+#   defp loop(count) do
+#     receive do
+#       {:admit,name} ->
+#         new_count = count + 1
+#         IO.puts("Зарегистрирован: #{name}. Всего пациентов: #{new_count}")
+#         loop(new_count) #Рекурсия! Процесс снова ждет сообщения с новым числом
+
+#       :get_count ->
+#         IO.puts("Сейчас в госпитале: #{count}")
+#         loop(count)
+
+#       {:check_age,age}->
+#         case Validator.check(%{age: age}) do
+#           {:ok, _} -> "Проходите"
+#           {:error, _} -> "Вам в десткое отделение"
+#         end
+#         loop(count)
+#     end
+#   end
+# end
--- syntax/hospital/mix.exs
+++ syntax/hospital/mix.exs
@@ -14,7 +14,8 @@ defmodule Hospital.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Hospital.Application, []} # <-- Эта строчка должна быть тут!
     ]
   end
 
